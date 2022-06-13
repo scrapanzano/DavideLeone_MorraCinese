97c2fd58cf3f6dfc02da4baaf40a952903918034
Scrapa <d.leone001@studenti.unibs.it>
2022-06-13 14:02:46 +0200

    Initial commit

---
+++ src/it/unibs/fp/morracinese/Main.java
@@ -0,0 +1,10 @@
+package it.unibs.fp.morracinese;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/it/unibs/fp/morracinese/Partita.java
@@ -0,0 +1,5 @@
+package it.unibs.fp.morracinese;
+
+public class Partita {
+
+}
